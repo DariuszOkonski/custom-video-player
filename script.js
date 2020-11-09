0644cc55dfd5e6236699c2f0ad97bf208fb26b60
Dariusz <darek200180@gmail.com>
2020-11-09 22:10:13 +0000

    add all event listeners

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+// play & pause viedo
+
+// event listener
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+stop.addEventListener('click', stopVideo);
+progress.addEventListener('change', setVideoProgress);
